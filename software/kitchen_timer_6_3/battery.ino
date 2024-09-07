7ed13d7d41116fed40ec602514c3418bc77a564e
Tom Briers <tbriers@telenet.be>
2024-09-07 09:30:30 +0200

    change to battery percentage

--- software/kitchen_timer_6_3/battery.ino
+++ software/kitchen_timer_6_3/battery.ino
@@ -15,7 +15,7 @@ void checkBatteryLevel() {
   batteryIndex++;
 
   if (batteryIndex == NUMBER_OF_BATTERY_SAMPLES) {
-    int averageBattery = 0;
+    float averageBattery = 0;
 
     for (int i = 0; i < NUMBER_OF_BATTERY_SAMPLES; i++) {
       averageBattery += batterySamples[i];
@@ -26,23 +26,28 @@ void checkBatteryLevel() {
   }
 }
 
-void setBatteryIndicator(int value) {
-  Serial.print("Set battery level ");
-  Serial.println(value);
+void setBatteryIndicator(float value) {
+  float voltage = value * 5.0 / 1023;
+  float percentage = mapf(voltage, 3.0, 4.2, 0, 100);
 
-  if (value > 825) {
-    Serial.println("Green");
+  Serial.print("voltage:");
+  Serial.print(voltage);
+  Serial.print(",percentage:");
+  Serial.println(percentage);
+
+  if (percentage > 75) {
     pixels.setPixelColor(0, pixels.Color(0, 255, 0));
-  } else if (value > 735) {
-    Serial.println("Yellow");
+  } else if (percentage > 50) {
     pixels.setPixelColor(0, pixels.Color(255, 255, 0));
-  } else if (value > 655) {
-    Serial.println("Orange");
+  } else if (percentage > 25) {
     pixels.setPixelColor(0, pixels.Color(255, 140, 0));
   } else {
-    Serial.println("Red");
     pixels.setPixelColor(0, pixels.Color(255, 0, 0));
   }
 
   pixels.show();
+}
+
+float mapf(float x, float in_min, float in_max, float out_min, float out_max) {
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
